749286cf180e012e3265f09d08dac7071cc40557
ubaid umer <ubaid.umer555@gmail.com>
2024-12-14 23:55:40 +0500

    resume updation

--- src/Pages/Resume/ResumeNew.jsx
+++ src/Pages/Resume/ResumeNew.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../../Components/Particle";
-import pdf from "../../Assets/Ubaid-UmerResume.pdf";
+import pdf from "../../Assets/UbaidUmer-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
