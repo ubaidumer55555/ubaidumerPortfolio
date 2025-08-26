6d1cb5faab718c07ec253a69b39b65216898954b
ubaid umer <ubaid.umer555@gmail.com>
2025-08-26 13:36:57 +0500

    added new resume file

--- src/Pages/Resume/ResumeNew.jsx
+++ src/Pages/Resume/ResumeNew.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../../Components/Particle";
-import pdf from "../../Assets/UbaidUmer-Resume.pdf";
+import pdf from "../../Assets/Ubaid_Umer_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
